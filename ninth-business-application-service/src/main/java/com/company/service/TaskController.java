d7c8ecba63f6ddbbf6f3546e36537a2b9dc9be8e
Karl Nicholas <karl.nicholas@outlook.com>
2019-10-09 08:04:21 -0700

    Updates.

--- ninth-business-application-service/src/main/java/com/company/service/TaskController.java
+++ ninth-business-application-service/src/main/java/com/company/service/TaskController.java
@@ -11,6 +11,7 @@ import org.jbpm.services.api.ProcessService;
 import org.jbpm.services.api.RuntimeDataService;
 import org.jbpm.services.api.UserTaskService;
 import org.jbpm.services.api.model.ProcessInstanceDesc;
+import org.jbpm.services.api.model.VariableDesc;
 import org.kie.api.runtime.query.QueryContext;
 import org.kie.api.task.model.Status;
 import org.kie.api.task.model.TaskSummary;
@@ -48,47 +49,68 @@ public class TaskController {
     
 	@RequestMapping({"/task"})
 	public String showTasks(Principal principal) {
+		
+		// create empty comment field
 		task.setComment(null);
+		
+		// make sure someone is logged in.
     	if ( principal != null ) {
-        	List<TaskSummary> taskSummaries = runtimeDataService.getTasksAssignedAsPotentialOwner(principal.getName(), new QueryFilter());
-        	if ( taskSummaries.size() > 0 ) {
-            	Status status = taskSummaries.get(0).getStatus();
-            	if ( status == Status.Reserved || status == Status.Ready )
-            		task.setTaskState(TASK_STATE.RESERVED);
-            	else if ( status == Status.InProgress )
-            		task.setTaskState(TASK_STATE.STARTED);
-        	} else {
-        		task.setTaskState(TASK_STATE.NONE);
-        	}
+    		
+    		// check if there are any tasks for this user
+        	checkForTasks(principal);
+        	
+        	// indicate whether this user is a member of HR
         	task.setMemberHr(hasRole("ROLE_HR"));
-        	if ( task.isMemberHr() ) {
-        		Collection<ProcessInstanceDesc> piDescs = runtimeDataService.getProcessInstances(new QueryContext());
-        		List<Eval> evals = new ArrayList<>();
-        		task.setEvals(evals);
-        		for ( ProcessInstanceDesc piDesc: piDescs ) {
-                	VariableInstanceList vil = ConvertUtils.convertToVariablesList(runtimeDataService.getVariablesCurrentState(piDesc.getId()));
-                	Eval eval = new Eval();
-                	evals.add(eval);
-					for ( VariableInstance vi : vil.getItems() ) {
-						switch( vi.getVariableName()) {
-						case "employee":
-							eval.setEmployee(vi.getValue());
-							break;
-						case "selfeval":
-							eval.setSelfeval(vi.getValue());
-							break;
-						case "pmeval":
-							eval.setPmeval(vi.getValue());
-							break;
-						case "hreval":
-							eval.setHreval(vi.getValue());
-							break;
-						}
-                	}
-        		}
-        	}
+        	
+        	// 
+        	displayTaskDataForHR();
+        	
     	}
+    	
+    	// display the task.html template with for above data
 	    return "task";
+	}
+
+	private void displayTaskDataForHR() {
+		if ( task.isMemberHr() ) {
+			Collection<ProcessInstanceDesc> piDescs = runtimeDataService.getProcessInstances(new QueryContext());
+			List<Eval> evals = new ArrayList<>();
+			task.setEvals(evals);
+			for ( ProcessInstanceDesc piDesc: piDescs ) {
+		    	Collection<VariableDesc> vil = runtimeDataService.getVariablesCurrentState(piDesc.getId());
+		    	Eval eval = new Eval();
+		    	evals.add(eval);
+				for ( VariableDesc vi : vil ) {
+					switch( vi.getVariableId()) {
+					case "employee":
+						eval.setEmployee(vi.getNewValue());
+						break;
+					case "selfeval":
+						eval.setSelfeval(vi.getNewValue());
+						break;
+					case "pmeval":
+						eval.setPmeval(vi.getNewValue());
+						break;
+					case "hreval":
+						eval.setHreval(vi.getNewValue());
+						break;
+					}
+		    	}
+			}
+		}
+	}
+
+	private void checkForTasks(Principal principal) {
+		List<TaskSummary> taskSummaries = runtimeDataService.getTasksAssignedAsPotentialOwner(principal.getName(), new QueryFilter());
+		if ( taskSummaries.size() > 0 ) {
+			Status status = taskSummaries.get(0).getStatus();
+			if ( status == Status.Reserved || status == Status.Ready )
+				task.setTaskState(TASK_STATE.RESERVED);
+			else if ( status == Status.InProgress )
+				task.setTaskState(TASK_STATE.STARTED);
+		} else {
+			task.setTaskState(TASK_STATE.NONE);
+		}
 	}
 
 	@RequestMapping(value="/task", method=RequestMethod.POST, params="action=startevaluation")
@@ -141,31 +163,4 @@ public class TaskController {
                 	return grantedAuthority.getAuthority().equals(roleName);	
                 });
     }
-/*	
-	@RequestMapping(value="/task", method=RequestMethod.POST, params="action=hrevaluation")
-    public ResponseEntity<Integer> hrEvaluation(Principal principal, @RequestParam String hreval) throws Exception {
-		Map<String, Object> params = new HashMap<>();
-		params.put("hreval", hreval);
-    	List<TaskSummary> taskSummaries = runtimeDataService.getTasksAssignedAsPotentialOwner(principal.getName(), new QueryFilter());
-    	taskSummaries.forEach(s->{
-    		userTaskService.claim(s.getId(), principal.getName());
-    		userTaskService.start(s.getId(), principal.getName());
-    		userTaskService.complete(s.getId(), principal.getName(), params);
-    	});
-    	return ResponseEntity.ok(taskSummaries.size());
-    }
-
-	@RequestMapping(value="/task", method=RequestMethod.POST, params="action=pmevaluation")
-    public ResponseEntity<Integer> pmEvaluation(Principal principal, @RequestParam String pmeval) throws Exception {
-		Map<String, Object> params = new HashMap<>();
-		params.put("pmeval", pmeval);
-    	List<TaskSummary> taskSummaries = runtimeDataService.getTasksAssignedAsPotentialOwner(principal.getName(), new QueryFilter());
-    	taskSummaries.forEach(s->{
-    		userTaskService.claim(s.getId(), principal.getName());
-    		userTaskService.start(s.getId(), principal.getName());
-    		userTaskService.complete(s.getId(), principal.getName(), params);
-    	});
-    	return ResponseEntity.ok(taskSummaries.size());
-    }
-*/
 }
