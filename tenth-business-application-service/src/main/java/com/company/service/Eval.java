ca1cd064691d7da81c716ebc2bde930924e7286f
Karl Nicholas <karl.nicholas@outlook.com>
2020-04-13 19:12:55 -0700

    Add tenth-business-application starting point.

---
+++ tenth-business-application-service/src/main/java/com/company/service/Eval.java
@@ -0,0 +1,33 @@
+package com.company.service;
+
+public class Eval {
+	private String employee;
+	private String selfeval;
+	private String pmeval;
+	private String hreval;
+	public String getEmployee() {
+		return employee;
+	}
+	public void setEmployee(String employee) {
+		this.employee = employee;
+	}
+	public String getSelfeval() {
+		return selfeval;
+	}
+	public void setSelfeval(String selfeval) {
+		this.selfeval = selfeval;
+	}
+	public String getPmeval() {
+		return pmeval;
+	}
+	public void setPmeval(String pmeval) {
+		this.pmeval = pmeval;
+	}
+	public String getHreval() {
+		return hreval;
+	}
+	public void setHreval(String hreval) {
+		this.hreval = hreval;
+	}
+
+}
